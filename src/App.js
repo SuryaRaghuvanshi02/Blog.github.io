10d6eb5a69013c62b3bf78fe69777cf7c3f52964
SuryaRaghuvanshi02 <73468842+SuryaRaghuvanshi02@users.noreply.github.com>
2023-05-02 17:33:53 +0530

    2nd commit with rough layout of the website

--- src/App.js
+++ src/App.js
@@ -10,6 +10,39 @@ function App() {
           <a href="">Register</a>
         </nav>
       </header>
+      <div className="post">
+          <div className="image"><img src="https://miro.medium.com/v2/resize:fit:640/format:webp/1*esLAtPgM_LTWzPYQfCQr8g.jpeg" alt="" /></div>
+          <div className="texts">
+            <h2>Link Prediction on Distributed Environment: A Comparison between Support Vector Machines and Neural Networks</h2>
+            <p className="info">
+              <a href="" className="autor">Vansh Gupta</a>
+              <time>2023-04-25 16:45</time>
+            </p>
+            <p className="summary">Websites are a ubiquitous aspect of our everyday lives in the modern world, from social media to itinerary.</p>
+          </div>
+        </div>
+        <div className="post">
+          <div className="image"><img src="https://miro.medium.com/v2/resize:fit:640/format:webp/1*esLAtPgM_LTWzPYQfCQr8g.jpeg" alt="" /></div>
+          <div className="texts">
+            <h2>Link Prediction on Distributed Environment: A Comparison between Support Vector Machines and Neural Networks</h2>
+            <p className="info">
+              <a href="" className="autor">Vansh Gupta</a>
+              <time>2023-04-25 16:45</time>
+            </p>
+            <p className="summary">Websites are a ubiquitous aspect of our everyday lives in the modern world, from social media to itinerary.</p>
+          </div>
+        </div>
+        <div className="post">
+          <div className="image"><img src="https://miro.medium.com/v2/resize:fit:640/format:webp/1*esLAtPgM_LTWzPYQfCQr8g.jpeg" alt="" /></div>
+          <div className="texts">
+            <h2>Link Prediction on Distributed Environment: A Comparison between Support Vector Machines and Neural Networks</h2>
+            <p className="info">
+              <a href="" className="autor">Vansh Gupta</a>
+              <time>2023-04-25 16:45</time>
+            </p>
+            <p className="summary">Websites are a ubiquitous aspect of our everyday lives in the modern world, from social media to itinerary.</p>
+          </div>
+        </div>
     </main>
   );
 }
